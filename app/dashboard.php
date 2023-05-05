5d25ff65f79cb48fc93516d6cc9a689bd6c75147
Denver Gregorio <gregoriomarkdenver01@gmail.com>
2023-05-05 09:58:39 +0800

    fix#2: remove summary of report in dashboard

--- app/dashboard.php
+++ app/dashboard.php
@@ -19,7 +19,6 @@ if($developer) {
 }
 ?>
 <main id="main" class="main">
-
   <div class="pagetitle">
     <h1>Dashboard</h1>
     <nav>
@@ -34,23 +33,20 @@ if($developer) {
    <?php if (isset($_SESSION['key_session']['user_level']) && ($_SESSION['key_session']['user_level'] == '1' || $_SESSION['key_session']['user_level'] == '2')) { ?>
     <div class="row">
 
-<!-- Left side columns -->
+    <!-- Left side columns -->
 <div class="col-lg-8">
-  <div class="row">
-  <div class="col-lg-12">
-  <?php echo display_message($msg); ?>
-    <?php if($developer) { ?>
-      <form method="POST">
-    <button name="developer_button" class="btn btn-lg btn-success rounded-0">DEVELOPER SIDE</button>
-    </form>
-    <?php } ?>
-  </div>
-  </div>
-
-
-
-  <div class="row"> 
-
+    <div class="row">
+      <div class="col-lg-12">
+        <?php echo display_message($msg); ?>
+        <?php if($developer) { ?>
+        <form method="POST">
+          <button name="developer_button" class="btn btn-lg btn-success rounded-0">DEVELOPER SIDE</button>
+        </form>
+        <?php } ?>
+      </div>
+    </div>
+    
+    <div class="row"> 
     <!-- Counseling Card -->
     <div class="col-xxl-6 col-md-6">
       <div class="card info-card customers-card rounded-0">
@@ -293,95 +289,9 @@ if($developer) {
 
 </div><!-- End number of visits Card -->
 
-    <!-- Reports -->
-    <div class="col-12">
-      <div class="card rounded-0">
 
-        <div class="filter">
-          <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-            <li class="dropdown-header text-start">
-              <h6>Filter</h6>
-            </li>
 
-            <li><a class="dropdown-item" href="#">Today</a></li>
-            <li><a class="dropdown-item" href="#">This Month</a></li>
-            <li><a class="dropdown-item" href="#">This Year</a></li>
-          </ul>
-        </div>
-
-        <div class="card-body">
-          <h5 class="card-title">Summary <span>/Report</span></h5>
-
-          <!-- Line Chart -->
-          <div id="reportsChart"></div>
-
-          <script>
-          document.addEventListener("DOMContentLoaded", () => {
-            new ApexCharts(document.querySelector("#reportsChart"), {
-              series: [{
-                name: 'Counseling',
-                data: [31, 40, 28, 51, 42, 82, 56],
-              }, {
-                name: 'Student',
-                data: [11, 32, 45, 32, 34, 52, 41]
-              },{
-                name: 'Excuse',
-                data: [11, 32, 45, 32, 34, 52, 41]
-              }, {
-                name: 'Visits',
-                data: [11, 32, 45, 32, 34, 52, 41]
-              },  {
-                name: 'Clearance',
-                data: [15, 11, 32, 18, 9, 24, 11]
-              }],
-              chart: {
-                height: 350,
-                type: 'area',
-                toolbar: {
-                  show: false
-                },
-              },
-              markers: {
-                size: 4
-              },
-              colors: ['#4154f1', '#2eca6a', '#ff771d', "#6a1b9a", "#f50057"],
-              fill: {
-                type: "gradient",
-                gradient: {
-                  shadeIntensity: 1,
-                  opacityFrom: 0.3,
-                  opacityTo: 0.4,
-                  stops: [0, 90, 100]
-                }
-              },
-              dataLabels: {
-                enabled: false
-              },
-              stroke: {
-                curve: 'smooth',
-                width: 2
-              },
-              xaxis: {
-                type: 'datetime',
-                categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-              },
-              tooltip: {
-                x: {
-                  format: 'dd/MM/yy HH:mm'
-                },
-              }
-            }).render();
-          });
-          </script>
-          <!-- End Line Chart -->
-
-        </div>
-
-      </div>
-    </div><!-- End Reports -->
-
-  </div>
+</div>
 </div><!-- End Left side columns -->
    <?php } else { ?>
 
