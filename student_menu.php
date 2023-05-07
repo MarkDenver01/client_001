61a0c786852549a73188318b6c2d1e02412427ff
Denver <gregoriomarkdenver01@gmail.com>
2023-05-07 15:48:56 +0800

    fix redirection

--- student_menu.php
+++ student_menu.php
@@ -187,7 +187,7 @@ if ($db->num_rows($result)) {
 </li>  
 <?php } elseif ($examinee['exam_title'] == 'Aptitude Verbal and Numerical') { ?>
 <li class="nav-item">
-  <a class="nav-link " href="../app/aptitude_verbal_n_numeric_result">
+  <a class="nav-link " href="../app/aptitude_verbal_n_numerical">
     <i class="ri-sticky-note-fill"></i>
     <span><?php echo $examinee['exam_title']; ?>'s Result</span>
   </a>
