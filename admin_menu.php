513a666f690a1fdea5981f33340613432bf1e2aa
Denver <gregoriomarkdenver01@gmail.com>
2023-05-08 03:44:29 +0800

    temporary disable the bulk upload

--- admin_menu.php
+++ admin_menu.php
@@ -51,17 +51,18 @@
         <span>Exam Management</span><i class="bi bi-chevron-down ms-auto"></i>
       </a>
       <ul id="icons-nav" class="nav-content collapsed" data-bs-parent="#sidebar-nav">
-        <li>
-          <a href="#" id="created_exam">
-            <i class="bi bi-circle"></i><span>Create an exam</span>
-          </a>
-        </li>
         <!-- <li>
-          <a href="./create_exam">
+          <a href="#" id="created_exam">
             <i class="bi bi-circle"></i><span>Create an exam</span>
           </a>
         </li> -->
         <li>
+          <a href="./create_exam">
+            <i class="bi bi-circle"></i><span>Create an exam</span>
+          </a>
+        </li>
+   
+        
         <li>
           <a href="./view_exam">
             <i class="bi bi-circle"></i><span>View exam record</span>
